2aff6d221fc2fbb6bf73d0d5493e093f83b57ad1
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-10-12 13:05:58 -0600

    re-organized paragraphs for arrays  in index.html; added content for arrays in client.js

--- client.js
+++ client.js
@@ -1,1 +1,11 @@
-const harryPotterCharacters = ["Harry", "Ron", "Hermione", "Hagrid" ,"Neville", "Sirius", "Lupin", "Dumbledore", "Snape"]
+const harryPotterCharacters = ["Harry", "Ron", "Hermione", "Hagrid" ,"Neville", "Sirius", "Lupin", "Dumbledore", "Snape"]; 
+
+//render const to html//
+document.getElementById("hp-characters").innerHTML = harryPotterCharacters;
+
+//Arrays//
+
+  //.push() method//
+   harryPotterCharacters.push("Draco");
+  //render to html//
+   
